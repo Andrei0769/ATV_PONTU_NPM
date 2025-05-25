d1c8eb2fa39430dcfc80b2f54a51c24a4ad487f2
Andrei Luiz de Jesus Souza Almeida <159576658+Andrei0769@users.noreply.github.com>
2025-05-24 23:03:49 -0300

    Add files via upload

---
+++ src/components/CadastroTreinador/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function CadastroTreinador() {
+  return <div className="container-treinador"></div>;
+}
+
+export default CadastroTreinador;
---
+++ src/pages/CadastroTreinador/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function PaginaCadastroTreinador() {
+  return <div className="pagina-cadastro"></div>;
+}
+
+export default PaginaCadastroTreinador;
